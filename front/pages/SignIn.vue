33108b9d06f5f62b07dee2638cdaff095746aa24
bbackjk <bbackjk1113@gmail.com>
2020-08-26 22:48:58 +0900

    [Front] Remove Validation Sign In

--- front/pages/SignIn.vue
+++ front/pages/SignIn.vue
@@ -1,24 +1,13 @@
 <template>
   <div class="container">
     <form>
-      <v-text-field
-        v-model="email"
-        :error-messages="emailErrors"
-        label="E-mail"
-        ref="email"
-        required
-        @input="$v.email.$touch()"
-        @blur="$v.email.$touch()"
-      ></v-text-field>
+      <v-text-field v-model="email" label="E-mail" ref="email" required></v-text-field>
       <v-text-field
         v-model="password"
-        :error-messages="passwordErrors"
         :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
         :type="show ? 'text' : 'password'"
         label="Password"
         required
-        @input="$v.password.$touch()"
-        @blur="$v.password.$touch()"
         @click:append="show = !show"
       ></v-text-field>
       <br />
@@ -26,9 +15,9 @@
       <v-btn @click="loginClear">CLEAR</v-btn>
       <br />
       <br />
-      <nuxt-link to="signup" class="signup-link"
-        ><span class="signup-text">Not Family? Go Sign Up</span></nuxt-link
-      >
+      <nuxt-link to="signup" class="signup-link">
+        <span class="signup-text">Not Family? Go Sign Up</span>
+      </nuxt-link>
 
       <Snackbar v-if="snackbar" :title="title" :snackbar-flag="snackbar" />
 
@@ -44,8 +33,6 @@
 </template>
 
 <script>
-import { validationMixin } from 'vuelidate';
-import { required, minLength, email } from 'vuelidate/lib/validators';
 import { mapState } from 'vuex';
 
 import Snackbar from '@/components/Snackbar.vue';
@@ -56,16 +43,6 @@ export default {
     Snackbar,
     Dialog,
   },
-  mixins: [validationMixin],
-
-  validations: {
-    password: {
-      required,
-      minLength: minLength(6),
-    },
-    email: { required, email },
-  },
-
   data: () => ({
     email: 'test8@example.com',
     password: 'pjkrud6839@@',
@@ -81,36 +58,12 @@ export default {
       message: (state) => state.user.message,
       loggedIn: (state) => state.user.loggedIn,
     }),
-    passwordErrors() {
-      const errors = [];
-
-      if (!this.$v.password.$dirty) return errors;
-
-      !this.$v.password.minLength &&
-        errors.push('Password is 6 characters at least.');
-
-      !this.$v.password.required && errors.push('Name is required.');
-
-      return errors;
-    },
-    emailErrors() {
-      const errors = [];
-
-      if (!this.$v.email.$dirty) return errors;
-
-      !this.$v.email.email && errors.push('Must be valid e-mail');
-
-      !this.$v.email.required && errors.push('E-mail is required');
-
-      return errors;
-    },
   },
 
   methods: {
     async loginSubmit() {
       this.snackbar = false;
       this.dialog = false;
-      this.$v.$touch();
       const data = {
         email: this.email,
         password: this.password,
@@ -145,7 +98,6 @@ export default {
     },
 
     loginClear() {
-      this.$v.$reset();
       this.password = '';
       this.email = '';
     },
